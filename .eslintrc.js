803231910b647bff1d1ef308705d2f3201ed8e73
Idan Rozin <idan@browzwear.com>
2022-07-05 11:42:31 +0300

    added eslint + prettier

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  env: {
+    es6: true,
+    browser: true,
+    es2021: true,
+  },
+  extends: ['airbnb-base', 'prettier'],
+  // parser: 'babel-eslint',
+  ignorePatterns: ['src/reportWebVitals.js'],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+      modules: true,
+      experimentalObjectRestSpread: true,
+    },
+  },
+  rules: {
+    'prettier/prettier': 'error',
+  },
+  plugins: ['prettier'],
+};
